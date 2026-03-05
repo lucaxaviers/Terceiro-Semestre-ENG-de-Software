ffc99ef122de186e2b72e89ab5e456efe0a39774
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2026-03-05 11:24:52 -0300

    Update and rename Novo Documento de Texto.txt to Exercicio.dart

---
+++ TECNOLOGIA E PROGRAMAÇÃO PARA DISPOSITIVOS MÓVEIS/Exercícios/Prática/Exercicio.dart
@@ -0,0 +1,9 @@
+abstract class MeioDeTransporte{}
+abstract class Terrestre extends MeioDeTransporte{}
+
+class Automovel extends Terrestre{}
+class Motocicleta extends Terrestre{}
+class Aeronave extends MeioDeTransporte{}
+class Embarcacao extends MeioDeTransporte{}
+
+// Fazer um main que crie uma frota
