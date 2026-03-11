45cbbf20876b8b915fb80d281d37e5245c04e3ef
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2026-03-11 19:10:27 -0300

    Create parouimpar.dart

---
+++ TECNOLOGIA E PROGRAMAÇÃO PARA DISPOSITIVOS MÓVEIS/Exercícios/Prática/Exercícios - Lab 1/parouimpar.dart
@@ -0,0 +1,17 @@
+/**
+Dado um número inteiro n, 
+imprima par se for par, 
+ou ímpar caso contrário.
+**/
+
+void main(){
+
+  int n = 7;
+
+  if (n % 2 == 0) {
+    print('par');
+  } else {
+    print('ímpar');
+  }
+
+}
