60b0c0f8240cf82346e772342580fc72da8e02bc
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2026-03-11 18:57:17 -0300

    Add inicio.dart to print user details

---
+++ TECNOLOGIA E PROGRAMAÇÃO PARA DISPOSITIVOS MÓVEIS/Exercícios/Prática/Exercícios - Lab 1/inicio.dart
@@ -0,0 +1,17 @@
+/**
+Crie um programa que imprima: 
+- seu nome
+- sua idade
+- sua turma
+(Os dados estarão em variáveis)
+**/
+void main(){
+  
+  String nome = 'Lucas Rodrigues Xavier';
+  int idade = 18;
+  int turma = 101;
+  
+  print('Nome: $nome');
+  print('Idade: $idade');
+  print('Turma: $turma');
+}
