6ef158a8b988df41aeea5eadb6523d0dad5ba467
Lucas Xavier <124785423+lucaxaviers@users.noreply.github.com>
2026-03-12 11:32:18 -0300

    Add files via upload

---
+++ TECNOLOGIA E PROGRAMAÇÃO PARA DISPOSITIVOS MÓVEIS/Aulas/PRÁTICA/Aula 5/Calculadora/Key.dart
@@ -0,0 +1,13 @@
+class Key {
+  final String label;
+  final KeyType type;
+  final String? value;
+  final CalcAction? action;
+
+  const Key({
+    required this.label,
+    required this.type,
+    this.value,
+    this.action,
+  });
+}
